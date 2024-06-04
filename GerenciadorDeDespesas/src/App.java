05b142aa8a492f21be4dda815da8d74678018851
Yris Gabrielle Sother Oliveira Pereira dos Reis <160602963+YrisSother@users.noreply.github.com>
2024-06-04 11:10:28 -0300

    Add files via upload

---
+++ GerenciadorDeDespesas/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        Usuario Bemvindo = new Usuario();
+        Bemvindo.nome();
+
+        sc.close();
+    }
+}
